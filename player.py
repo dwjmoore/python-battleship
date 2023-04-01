23c3518ce26cfcb642526360a36d31709ae6ec54
dwjmoore <>
2023-04-01 02:11:06 +0000

    added Player and Ship class with a few prelim methods

---
+++ player.py
@@ -0,0 +1,9 @@
+class Player:
+	def __init__(self):
+		pass
+	
+	def attack(self, other_player):
+		pass
+		
+	def place_ship(self, ship):
+		pass
---
+++ ship.py
@@ -0,0 +1,6 @@
+class Ship:
+	def __init__(self, length):
+		self.length = length
+
+	def take_a_hit(self):
+		pass
