e638d89bcfa97a41af39bfa45ab1883cc18a8e38
dwjmoore <>
2023-05-19 20:33:14 +0000

    added a symbol variable to the ship class

--- ship.py
+++ ship.py
@@ -3,12 +3,19 @@ class Ship:
 		self.type = type
 		if self.type == 'carrier':
 			self.length = 5
+			self.symbol = 'A'
 		if self.type == 'battleship':
 			self.length = 4
-		if self.type == 'cruiser' or self.type == 'submarine':
+			self.symbol = 'B'
+		if self.type == 'cruiser'
 			self.length = 3
+			self.symbol = 'C'
+		if self.type == 'submarine':
+			self.length = 3
+			self.symbol = 'S'
 		if self.type == 'destroyer':
 			self.length = 2
+			self.symbol = 'D'
 		self.location = []
 
 	def take_a_hit(self):
