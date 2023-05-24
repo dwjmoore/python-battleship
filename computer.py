2da3cc8c143efcc52d39707ec522c0ea25cd076a
dwjmoore <>
2023-05-24 14:52:18 +0000

    created method to get attack coord after computer gets two hits in a row

--- computer.py
+++ computer.py
@@ -12,8 +12,7 @@ class Computer:
 		self.just_sunk_ship = False
 		self.found_ship = False
 
-	def attack(self, enemy_ships_board, player_enemy_board, enemy_ships,
-	           enemy_player):
+	def attack(self, enemy_ships_board, player_enemy_board, enemy_ships, enemy_player):
 		print("Player 2 attacks:")
 		self.attack_number += 1
 		#Computer inputs attack coordinate
@@ -57,6 +56,10 @@ class Computer:
 		if self.attack_number == 1 or self.just_sunk_ship == True:
 			self.just_sunk_ship = False
 			return (random.choice(letters) + random.choice(numbers))
+
+		if self.attack_number >= 3 and self.attack_hit_log[-1] == True and self.attack_hit_log[-2] == True and self.found_ship == True:
+			return self.get_attack_coord_after_two_hits()
+
 		if self.attack_number >= 2 and self.attack_hit_log[-1] == True:
 			return self.get_attack_coord_after_hit(-1, player_enemy_board)
 		if self.attack_number >= 3 and self.attack_hit_log[
@@ -71,12 +74,6 @@ class Computer:
 		  -1] == False and self.attack_hit_log[-2] == False and self.attack_hit_log[
 		   -3] == False and self.attack_hit_log[-4] and self.found_ship == True:
 			return self.get_attack_coord_after_hit(-4, player_enemy_board)
-
-		
-		if self.attack_number >= 4 and self.attack_hit_log[
-		  -1] == True and self.attack_hit_log[-2] == False and self.attack_hit_log[
-		   -3] == True:
-			pass
 		return (random.choice(letters) + random.choice(numbers))
 
 	def check_attack_coord_validity(self, coord, player_enemy_board):
@@ -92,8 +89,7 @@ class Computer:
 		print(f"{attack_coord} is a miss.")
 		return False
 
-	def add_X_to_ship_and_check_if_sunk(self, attack_coord, enemy_ships,
-	                                    enemy_player):
+	def add_X_to_ship_and_check_if_sunk(self, attack_coord, enemy_ships, enemy_player):
 		for ship in enemy_ships:
 			for coord in ship.location:
 				if coord == attack_coord:
@@ -295,7 +291,7 @@ class Computer:
 			attack_coord_options.append(
 			 numbers_to_letters[letters_to_numbers[last_hit_coord_letter] + 1] +
 			 str(last_hit_coord_number))
-			#Gets attack coord option along the column
+		#Gets attack coord option along the column
 		if last_hit_coord_number == 1:
 			attack_coord_options.append(last_hit_coord_letter +
 																	str(last_hit_coord_number + 1))
@@ -334,3 +330,27 @@ class Computer:
 			return (random.choice(letters) + random.choice(numbers))
 		#Randomly gets attack coord from attack coord options list
 		return random.choice(attack_coord_options)
+
+	def get_attack_coord_after_two_hits(self):
+		first_hit_coord_letter = self.attack_coord_log[-2][0]
+		first_hit_coord_number = int(self.attack_coord_log[-2][1:])
+		second_hit_coord_letter = self.attack_coord_log[-1][0]
+		second_hit_coord_number = int(self.attack_coord_log[-1][1:])
+		if first_hit_coord_letter == second_hit_coord_letter:
+			if second_hit_coord_number == 1:
+				return (first_hit_coord_letter + str(first_hit_coord_number + 1))
+			if second_hit_coord_number == 10:
+				return (first_hit_coord_letter + str(first_hit_coord_number - 1))
+			if first_hit_coord_number > second_hit_coord_number:
+				return (first_hit_coord_letter + str(second_hit_coord_number - 1))
+			if first_hit_coord_number < second_hit_coord_number:
+				return (first_hit_coord_letter + str(second_hit_coord_number + 1))
+		if first_hit_coord_number == second_hit_coord_number:
+			if second_hit_coord_letter == 'A':
+				return (chr(ord(first_hit_coord_letter) + 1) + str(first_hit_coord_number))
+			if second_hit_coord_letter == 'J':
+				return (chr(ord(first_hit_coord_letter) - 1) + str(first_hit_coord_number))
+			if first_hit_coord_letter > second_hit_coord_letter:
+				return (chr(ord(second_hit_coord_letter) - 1) + str(first_hit_coord_number))
+			if first_hit_coord_letter < second_hit_coord_letter:
+				return (chr(ord(second_hit_coord_letter) + 1) + str(first_hit_coord_number))
